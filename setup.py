80b4b85e19a606e9e134acdeb6c59316da844b95
= <colinharris05@gmail.com>
2016-03-06 22:59:15 -0600

    ho

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import os
 
 dotfilesPath = './dotfiles/'
@@ -6,3 +5,6 @@ dotfilesPath = './dotfiles/'
 for filename in os.listdir(dotfilesPath):
   print "Copying " + dotfilesPath + filename + " to ~/." + filename
   os.system("cp -r " + dotfilesPath + filename + " ~/." + filename)
+  if (filename == 'bashrc') | (filename == 'bash_profile'):
+    print "Source ~/." + filename
+    os.system("source ~/." + filename)
